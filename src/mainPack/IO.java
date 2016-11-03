78eabcc3ca637aa0ac4e79355970977143e7bc95
Comingdonut <jamescastrejon12@gmail.com>
2016-11-03 08:30:53 -0600

    Minor Print Changes

--- src/mainPack/IO.java
+++ src/mainPack/IO.java
@@ -2,8 +2,12 @@ package mainPack;
 
 public class IO {
 	
+	public void printUnknownCommandError(){
+		System.err.println("I don't know how to do that.");
+	}
+	
 	public void printCommandError(Action a, Item b){
-		System.out.println("The command [" + a.toString() + "] is not capatible with [" + b.toString() + "].");
+		System.err.println("The command [" + a.toString() + "] is not capatible with [" + b.toString() + "].");
 	}
 	
 	public void printError(String error){
