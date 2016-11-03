ec7804afccedf81e40dee88cb9294108a3d696ff
Comingdonut <jamescastrejon12@gmail.com>
2016-11-03 08:57:47 -0600

    Minor changes and fixes

--- src/mainPack/IO.java
+++ src/mainPack/IO.java
@@ -2,19 +2,19 @@ package mainPack;
 
 public class IO {
 	
-	public void printUnknownCommandError(){
-		System.err.println("I don't know how to do that.");
+	public static void printUnknownCommandError(){
+		System.err.println("I don't know how to do that..");
 	}
 	
-	public void printCommandError(Action a, Item b){
-		System.err.println("The command [" + a.toString() + "] is not capatible with [" + b.toString() + "].");
+	public static void printCommandError(Action a, Item b){
+		System.err.println("The command [" + a.toString() + "] is not compatible with [" + b.toString() + "].");
 	}
 	
-	public void printError(String error){
+	public static void printError(String error){
 		System.err.println(error);
 	}
 	
-	public void print(String message){
+	public static void print(String message){
 		System.out.println(message);
 	}
 }
